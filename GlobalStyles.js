1532705781ca4726955c39ef859549cf29db443e
Ian Salt <ian.salt@iris-worldwide.com>
2020-10-14 20:27:07 +0100

    global style updates

--- GlobalStyles.js
+++ GlobalStyles.js
@@ -2,11 +2,16 @@ import react from 'react'
 import { StyleSheet } from 'react-native'
 
 const styles = StyleSheet.create({
+  /* creating a style guide for the entire project...  the theory is to use only the global styles */
+  /* local styles to be applied within a created component or screen folder so as not to affect globals */
+
+  /* colors */
   darkFieldGreen: {color: '#002210'},
   buttonGreen: {color: '#019244'},
   textGreen: {color:'#11CB52'},
   footerTextgGreen: {color: '#023017'},
 
+  /* headers */
   h1: { fontSize: 32, fontWeight: 'bold' },
   h2: { fontSize: 24, fontWeight: 'bold' },
   h3: { fontSize: 20, fontWeight: 'bold' },
@@ -14,14 +19,11 @@ const styles = StyleSheet.create({
   h5: { fontSize: 12, fontWeight: 'bold' },
   h6: { fontSize: 10, fontWeight: 'bold' },
 
-  flatListStyle: {
-    fontSize: 12,
-    marginVertical: 5,
-  },
+  /* styles for various elements */
+  flatListStyle: { fontSize: 12, marginVertical: 5, },
 
-  mainWrapper: {
-    padding: 10,
-  }
+  /* wrapper styles */
+  mainWrapper: { padding: 10, },
 });
 
 export default styles
