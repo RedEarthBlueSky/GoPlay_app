857c3318d64868ec2db75501ec544fa233b0a108
Ian Salt <ian.salt@iris-worldwide.com>
2020-10-14 20:29:36 +0100

    global style updates

--- GlobalStyles.js
+++ GlobalStyles.js
@@ -3,9 +3,10 @@ import { StyleSheet } from 'react-native'
 
 const styles = StyleSheet.create({
   /* creating a style guide for the entire project...  the theory is to use only the global styles */
-  /* local styles to be applied within a created component or screen folder so as not to affect globals */
+  /* the theory is to create a list of styles that mirror the out of the box functionality of html tags */
+  /* local styles to be applied within a seperate component or screen sub folder so as to overridge but not affect globals */
 
-  /* colors */
+  /* default colors */
   darkFieldGreen: {color: '#002210'},
   buttonGreen: {color: '#019244'},
   textGreen: {color:'#11CB52'},
@@ -19,7 +20,7 @@ const styles = StyleSheet.create({
   h5: { fontSize: 12, fontWeight: 'bold' },
   h6: { fontSize: 10, fontWeight: 'bold' },
 
-  /* styles for various elements */
+  /* list styles */
   flatListStyle: { fontSize: 12, marginVertical: 5, },
 
   /* wrapper styles */
